58a256470ad459f28350d927315fc8044a8ce2c6
ramandeva <42244517+ramandeva@users.noreply.github.com>
2018-12-25 12:52:27 +0800

    Add files via upload

---
+++ js/settings.js
@@ -0,0 +1,8 @@
+﻿var whSettings = {
+	highlightSearchWords : false,
+	highlightSearchWordsSingleColor : false,
+	tocOnlyExpandSingleHeading : false,
+	tocAutoExpandOnActivate : true,
+	tocAtimateExpandingCollapsingDuration : 50,
+	indexFile : 'index.htm'
+}
